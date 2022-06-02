cab2749820b2102817367fa6a04ad8e70062b53d
ludooo2204 <vachon.ludovic@gmail.com>
2022-06-02 22:01:55 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div className={styles.un}>1</div>
+      <div className={styles.deux}>2</div>
+      <div className={styles.trois}>3</div>
+      <div className={styles.quatre}>4</div>
+      <div className={styles.cinq}>5</div>
+      <div className={styles.six}>6</div>
     </div>
   );
 }
